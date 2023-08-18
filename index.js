86cadc44f61a763800d92fe507ea937063ea0807
Joseph <joseph.enrique321@gmail.com>
2023-08-17 20:53:40 -0500

    Valores actualizados Parametros

--- index.js
+++ index.js
@@ -1,22 +1,71 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port =3000;
+const PORT = 3000;
 
-app.get('/',(req,res) => {
-  res.send('Hola mi server');
-})
-
-app.get('/nueva-ruta',(req,res) => {
-  res.send('Hola mi server,soy nueva ruta');
-})
-
-app.get('/producto',(req,res) => {
-  res.json({
-    name:'Product 1',
-    price: 1000,
-  });
-})
-
-app.listen(port,()=>{
-  console.log('Mi port'+port);
+app.get('/', (req, res) => {
+    res.send('Hello ');
 });
+
+app.get('/route-new', (req, res) => {
+    res.send('Hola soy una nueva ruta o endpoint');
+});
+
+app.get('/products', (req, res) => {
+    res.json([{
+            name: 'Laptop Gamer',
+            price: 23000,
+        },
+        {
+            name: 'iPhone X3',
+            price: 32000,
+        }
+    ]);
+});
+
+app.get('/products/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({
+        id,
+        name: 'iPhone X3',
+        price: 32000,
+    });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+    const { categoryId, productId } = req.params;
+    res.json({
+        categoryId,
+        productId
+    });
+});
+
+app.get('/categories/:categoryId', (req, res) => {
+    const { categoryId } = req.params;
+    res.json({
+        categoryId,
+        category: 'Computers & Accesories'
+    });
+});
+
+app.get('/people', (req, res) => {
+    res.json([{
+        name: 'Arturo',
+        type: 'employee'
+    }, {
+        name: 'Jimena',
+        type: 'customer'
+    }]);
+});
+
+app.get('/people/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({
+        id,
+        name: 'Arturo',
+        type: 'employee'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log('Mi puerto es: ' + PORT);
+})
